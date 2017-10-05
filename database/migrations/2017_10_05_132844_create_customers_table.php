e1c832f6caf66ccd500f9913ffe0ef21960f4d9f
Tobias Wiklund <tobias.wiklund@gmail.com>
2017-10-05 18:25:32 +0200

    added unique key on customer email

--- database/migrations/2017_10_05_132844_create_customers_table.php
+++ database/migrations/2017_10_05_132844_create_customers_table.php
@@ -16,7 +16,7 @@ class CreateCustomersTable extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 191);
-            $table->string('email', 191);
+            $table->string('email', 191)->unique();
             $table->string('password', 255);
             $table->timestamps();
         });
