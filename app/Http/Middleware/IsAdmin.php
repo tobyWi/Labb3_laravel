7988dea8ee20f10d174de311abfe6031959f5765
Tobias Wiklund <tobias.wiklund@gmail.com>
2017-10-06 12:55:16 +0200

    added middleware for admin

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -3,24 +3,18 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use \Illuminate\Support\Facades\Auth;
 
 class IsAdmin
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
+
     public function handle($request, Closure $next)
     {
-        if(Auth::user()->is_admin == 1) {
+        if(Auth::user()->is_admin) {
 
             return $next($request);
         }
 
         return redirect('/');
-        
     }
 }
